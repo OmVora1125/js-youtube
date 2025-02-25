818adee0346fd92618b4a50ba92ccd168266ba19
OmVora1125 <omvora229@gmail.com>
2025-02-25 12:48:39 +0000

    Intro

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 23498
+var accountEmail = "om@gmail.com"
+let accountPass = "12345"
+accountCity = "goa"
+let accountState
+//accountId = 52 can't be changed
+
+/* var and let can be changed
+never use var as it don't understand scopes......
+*/
+accountEmail = "hello@gmail,com"
+accountPass = "14589"
+accountCity = "MUMBAI"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPass, accountCity, accountState])
