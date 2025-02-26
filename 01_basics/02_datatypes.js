851d829b3540335a04edef476528dd19e049124e
OmVora1125 <omvora229@gmail.com>
2025-02-26 10:27:54 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treats all JS code as a newer version
+
+// alert(3 + 3) // can't be used as we are using node JS, not browser
+
+let name = "om"
+let age = 20
+let isLoggedIn = true
+
+// number
+// bigint
+// String 
+// boolean
+// null -- has value assigned as null
+// undefined -- no value assigned
+// symbol -- unique 
+
+// object
+
+console.log(typeof "om");
+console.log(typeof age);
+
+console.log(typeof null); // output is object
+console.log(typeof undefined); // Output is undefined
